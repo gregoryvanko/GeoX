32f964eacb72209ae4daeaae41a93085b414bae7
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2022-01-24 23:39:43 +0100

    Splashscreen and HomePage

--- exemple.js
+++ exemple.js
@@ -1,6 +1,12 @@
-let GeoX = require('./index').GeoX
-const Name = "GeoXDev"
 const Port = 5000
+const Name = "GeoXDev"
 const Debug = true
-let MyApp = new GeoX(Name, Port, Debug)
-MyApp.Start()
+require('./index').Start(Port, Name, Debug)
+
+
+// let GeoX = require('./index').GeoX
+// const Name = "GeoXDev"
+// const Port = 5000
+// const Debug = true
+// let MyApp = new GeoX(Name, Port, Debug)
+// MyApp.Start()
